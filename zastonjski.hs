82480422acd10702f8a2d1eac1f4684ea8426920
Domen <domen.monik@gmail.com>
2014-12-30 19:31:52 +0100

    Popravljena funkcija getFormula. Popravljena podatkovni tip relacije in formule. V funkciji getFormula je potrebno še poskrbeti za avtomatsko generiranje spremenljivk.

--- zastonjski.hs
+++ zastonjski.hs
@@ -1,55 +1,102 @@
+-- | Program za generiranje zastonjskih izrekov za tipe
+
 import Data.List
 import Data.Maybe (fromJust)
 
+-- | Spremenljivka, ki predstavlja tip
 type TypeVariable = String
 
+-- | Tip
 data Type
-	= TypeVar TypeVariable
-	| TypeFun Type Type
+	= TypeVar TypeVariable  -- ^ Spremenljivka, ki predstavlja tip
+	| TypeFun Type Type     -- ^ Funkcijski konstruktor tipov ->
 	deriving (Show, Eq)
 
+-- | Spremenljivka, ki predstavlja izraz
 type TermVariable = String
-	
+
+-- | Izraz
 data Term
-	= TermVar TermVariable
-	| TermApp Term Term
+	= TermVar TermVariable  -- ^ Spremenljivka, ki predstavlja izraz
+	| TermApp Term Term     -- ^ Aplikacija izraza na izraz
 	deriving Show
 
-
+-- | Spremenljivka, ki predstavlja relacijo
 type RelationVariable = String
 
-data Relation
-	= RelVar RelationVariable Type Type
-	| RelFun Relation Relation
-	deriving Show
+-- | Osnovna relacija med dvema tipoma
+data Relation = Relation { associatedType :: TypeVariable   -- ^ spremenljivka tipa, na katero se nanaša relacija
+                         , relvar :: RelationVariable       -- ^ spremenljivka, ki predstavlja relacijo
+                         , leftType :: TypeVariable         -- ^ spremenljivka levega tipa
+                         , rightType :: TypeVariable        -- ^ spremenljivka desnega tipa
+                         } deriving Show
 
-	
+-- | Logična formula, ki predstavlja zastonjski izrek
 data Formula
-	= ForallRelations Relation Formula
-	| ForallPairs (TermVariable, TermVariable) Relation Formula
-	| IsMember (Term, Term) Relation
-	| Equivalence Formula Formula
-	| Implication Formula Formula
+	= ForallRelations RelationVariable (TypeVariable, TypeVariable) Formula -- ^ Kvantificira relacijsko spremenljivko in dva tipa
+	| ForallFunctions TermVariable Type Formula                             -- ^ Kvantificira funkcijsko spremenljivko in funkcijski tip (konstruiran iz TypeFun)
+	| ForallPairs (TermVariable, TermVariable) RelationVariable Formula     -- ^ Kvantificira dve izrazovni spremenljivki
+	| IsMember (Term, Term) RelationVariable                                -- ^ Par izrazov je v relaciji
+	| Implication Formula Formula                                           -- ^ Iz prve formule sledi druga formula
 	deriving Show
 
+-- | Shramba spremenljivk za relacije	
+relVarStore = ["R","S","T","U","V","Z"]
 
-createBasicRelations :: Int -> [Relation]
-createBasicRelations n = zipWith3 RelVar (take n ["R","S","T","U","V","Z"]) (take n (map TypeVar $ map ("t" ++) $ map (\x -> show x) [1,3..])) (take n (map TypeVar $ map ("t" ++) $ map (\x -> show x) [2,4..]))
+-- | Shramba spremenljivk za funkcije
+funVarStore = ["p","q","g","h","r","s","k","l","i","j","t","o"]
 
-getTypeVariables :: Type -> [TypeVariable]
-getTypeVariables (TypeVar x) = [x]
-getTypeVariables (TypeFun a b) = nub $ concat [getTypeVariables a, getTypeVariables b]
+-- | Shramba spremenljivk za izraze
+termVarStore = ["x","y","u","v","w","z","a","b","c","d","m","n"]
 
-createRelation :: Type -> Relation
-createRelation ty =
-    let vars = getTypeVariables ty
-        rels = createBasicRelations $ length vars
-    in case ty of
-        TypeVar tyvar -> rels !! (fromJust $ elemIndex tyvar vars)
-        TypeFun ty1 ty2 -> RelFun (createRelation ty1) (createRelation ty2)
-
+-- | Za dani tip vrne pripadajoči zastonjski izrek
 getFormula :: Type -> Formula
-getFormula ty = foldr ForallRelations (IsMember (TermVar "f", TermVar "f") (createRelation ty)) (createBasicRelations (length $ getTypeVariables ty))
+getFormula ty = fold relations (formula (TermVar "f") (TermVar "f") ty) where
+    allTypeVariables' typ =
+        case typ of
+            TypeVar x -> [x]
+            TypeFun a b -> nub $ concat [allTypeVariables' a, allTypeVariables' b]
+    allTypeVariables = allTypeVariables' ty
+    n = length allTypeVariables
+    relations' tvars =
+        case tvars of
+            [] -> []
+            v:vs -> Relation { associatedType = v
+                             , relvar = relVarStore !! (n-j-1)
+                             , leftType = "t" ++ (show $ 2*(n-j)-1)
+                             , rightType = "t" ++ (show $ 2*(n-j))
+                             } : (relations' vs)
+                             where j = length vs
+    relations = relations' allTypeVariables
+    fold rels frm =
+        case rels of
+            [] -> frm
+            r:rs -> ForallRelations (relvar r) (leftType r, rightType r) $ fold rs frm
+    associatedRelation' tvar rels =
+        case rels of
+            r:rs -> if (associatedType r) == tvar then r else associatedRelation' tvar rs
+    associatedRelation tvar = associatedRelation' tvar relations
+    assocRelVar tvar = relvar $ associatedRelation tvar
+    associatedFunction1 typ =
+        case typ of
+            TypeVar v -> TypeVar $ leftType $ associatedRelation v
+            TypeFun a b -> TypeFun (associatedFunction1 a) (associatedFunction1 b)
+    associatedFunction2 typ =
+        case typ of
+            TypeVar v -> TypeVar $ rightType $ associatedRelation v
+            TypeFun a b -> TypeFun (associatedFunction2 a) (associatedFunction2 b)
+    formula term1 term2 typ =
+        case typ of
+            TypeVar var -> IsMember (term1, term2) $ assocRelVar var
+            TypeFun (TypeVar avar) b -> ForallPairs ("x", "y") (assocRelVar avar) $ formula (TermApp term1 $ TermVar "x") (TermApp term2 $ TermVar "y") b -- TODO: namesto x in y naj bosta generirani spremenljivki iz shrambe termVarStore
+            TypeFun a b -> ForallFunctions "p" -- TODO: namesto spremenljivk p in q naj bosta generirani spremenljivki iz shrambe funVarStore
+                                           (associatedFunction1 a)
+                                           (ForallFunctions "q"
+                                                            (associatedFunction2 a)
+                                                            (Implication (formula (TermVar "p") (TermVar "q") a)
+                                                                         (formula (TermApp term1 $ TermVar "p") (TermApp term2 $ TermVar "q") b)
+                                                            )
+                                           )
 
-t :: Type
+t :: Type -- primer izraza (aplikacija) ; za poiskus zaženi getFormula t
 t = TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun (TypeVar "x") (TypeVar "y") )
