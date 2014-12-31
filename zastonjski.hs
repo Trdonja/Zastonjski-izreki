54ef96bf9f30cec74e1e23dc01b3b30b7264f5e2
Domen <domen.monik@gmail.com>
2014-12-31 17:08:25 +0100

    Prva delujoča različica programa. Vključen generator spremenljivk za izraze in funkcije. Napisane instance za Show.

--- zastonjski.hs
+++ zastonjski.hs
@@ -1,7 +1,8 @@
--- | Program za generiranje zastonjskih izrekov za tipe
-
 import Data.List
 import Data.Maybe (fromJust)
+
+-- | Program za generiranje zastonjskih izrekov za tipe
+-- module FreeTheorems where
 
 -- | Spremenljivka, ki predstavlja tip
 type TypeVariable = String
@@ -10,7 +11,10 @@ type TypeVariable = String
 data Type
 	= TypeVar TypeVariable  -- ^ Spremenljivka, ki predstavlja tip
 	| TypeFun Type Type     -- ^ Funkcijski konstruktor tipov ->
-	deriving (Show, Eq)
+
+instance Show Type where
+    show (TypeVar v) = v
+    show (TypeFun t1 t2) = (show t1) ++ " -> " ++ (show t2)
 
 -- | Spremenljivka, ki predstavlja izraz
 type TermVariable = String
@@ -19,7 +23,10 @@ type TermVariable = String
 data Term
 	= TermVar TermVariable  -- ^ Spremenljivka, ki predstavlja izraz
 	| TermApp Term Term     -- ^ Aplikacija izraza na izraz
-	deriving Show
+
+instance Show Term where
+    show (TermVar v) = v
+    show (TermApp t1 t2) = (show t1) ++ " " ++ (show t2)
 
 -- | Spremenljivka, ki predstavlja relacijo
 type RelationVariable = String
@@ -29,7 +36,7 @@ data Relation = Relation { associatedType :: TypeVariable   -- ^ spremenljivka t
                          , relvar :: RelationVariable       -- ^ spremenljivka, ki predstavlja relacijo
                          , leftType :: TypeVariable         -- ^ spremenljivka levega tipa
                          , rightType :: TypeVariable        -- ^ spremenljivka desnega tipa
-                         } deriving Show
+                         }
 
 -- | Logična formula, ki predstavlja zastonjski izrek
 data Formula
@@ -38,20 +45,31 @@ data Formula
 	| ForallPairs (TermVariable, TermVariable) RelationVariable Formula     -- ^ Kvantificira dve izrazovni spremenljivki
 	| IsMember (Term, Term) RelationVariable                                -- ^ Par izrazov je v relaciji
 	| Implication Formula Formula                                           -- ^ Iz prve formule sledi druga formula
-	deriving Show
+
+instance Show Formula where
+    show (ForallRelations rv (tv1, tv2) fml) = "Forall " ++ rv ++ " between " ++ tv1 ++ " x " ++ tv2 ++ "\n" ++ (show fml)
+    show (ForallFunctions tv ty fml) = "Forall " ++ tv ++ " :: " ++ (show ty) ++ "\n" ++ (show fml)
+    show (ForallPairs (tv1, tv2) rv fml) = "Forall (" ++ tv1 ++ ", " ++ tv2 ++ ") in " ++ rv ++ " . " ++ (show fml)
+    show (IsMember (term1, term2) rv) = "(" ++ (show term1) ++ ", " ++ (show term2) ++ ") in " ++ rv
+    show (Implication fml1 fml2) = "(" ++ (show fml1) ++ ") => (" ++ (show fml2) ++ ")"
 
 -- | Shramba spremenljivk za relacije	
 relVarStore = ["R","S","T","U","V","Z"]
 
--- | Shramba spremenljivk za funkcije
-funVarStore = ["p","q","g","h","r","s","k","l","i","j","t","o"]
+-- | Generator spremenljivk za funkcije in izrazre
+data VarGenerator = VarGenerator {funVarStore :: [String], termVarStore :: [String]}
 
--- | Shramba spremenljivk za izraze
-termVarStore = ["x","y","u","v","w","z","a","b","c","d","m","n"]
+-- | Generira par izrazovnih spremenljivk in vrne nov generator
+nextTermVar :: VarGenerator -> (String, String, VarGenerator)
+nextTermVar vg = let tv = termVarStore vg in (head tv, head $ tail tv, VarGenerator {funVarStore = funVarStore vg, termVarStore = tail $ tail tv})
+
+-- | Generira par funkcijskih spremenljivk in vrne nov generator
+nextFunVar :: VarGenerator -> (String, String, VarGenerator)
+nextFunVar vg = let fv = funVarStore vg in (head fv, head $ tail fv, VarGenerator {funVarStore = tail $ tail fv, termVarStore = termVarStore vg})
 
 -- | Za dani tip vrne pripadajoči zastonjski izrek
 getFormula :: Type -> Formula
-getFormula ty = fold relations (formula (TermVar "f") (TermVar "f") ty) where
+getFormula ty = fold relations (fst $ formula (TermVar "f") (TermVar "f") ty varGenerator) where
     allTypeVariables' typ =
         case typ of
             TypeVar x -> [x]
@@ -85,18 +103,28 @@ getFormula ty = fold relations (formula (TermVar "f") (TermVar "f") ty) where
         case typ of
             TypeVar v -> TypeVar $ rightType $ associatedRelation v
             TypeFun a b -> TypeFun (associatedFunction2 a) (associatedFunction2 b)
-    formula term1 term2 typ =
+    varGenerator = VarGenerator { funVarStore = ["p","q","r","s","g","h","k","l","i","j","t","o"] -- | Shramba spremenljivk za funkcije
+                                , termVarStore = ["x","y","u","v","w","z","a","b","c","d","m","n"] -- | Shramba spremenljivk za izraze
+                                }
+    formula term1 term2 typ gen =
         case typ of
-            TypeVar var -> IsMember (term1, term2) $ assocRelVar var
-            TypeFun (TypeVar avar) b -> ForallPairs ("x", "y") (assocRelVar avar) $ formula (TermApp term1 $ TermVar "x") (TermApp term2 $ TermVar "y") b -- TODO: namesto x in y naj bosta generirani spremenljivki iz shrambe termVarStore
-            TypeFun a b -> ForallFunctions "p" -- TODO: namesto spremenljivk p in q naj bosta generirani spremenljivki iz shrambe funVarStore
-                                           (associatedFunction1 a)
-                                           (ForallFunctions "q"
-                                                            (associatedFunction2 a)
-                                                            (Implication (formula (TermVar "p") (TermVar "q") a)
-                                                                         (formula (TermApp term1 $ TermVar "p") (TermApp term2 $ TermVar "q") b)
-                                                            )
-                                           )
+            TypeVar var -> (IsMember (term1, term2) $ assocRelVar var, gen)
+            TypeFun (TypeVar avar) b ->
+                let (x, y, newgen) = nextTermVar gen
+                    (endFormula, newgen1) = formula (TermApp term1 $ TermVar x) (TermApp term2 $ TermVar y) b newgen
+                in (ForallPairs (x, y) (assocRelVar avar) endFormula, newgen1)
+            TypeFun a b ->
+                let (p, q, newgen1) = nextFunVar gen
+                    (fstFormula, newgen2) = formula (TermVar p) (TermVar q) a newgen1
+                    (sndFormula, newgen3) = formula (TermApp term1 $ TermVar p) (TermApp term2 $ TermVar q) b newgen2
+                in (ForallFunctions p (associatedFunction1 a) $ ForallFunctions q (associatedFunction2 a) $ Implication fstFormula sndFormula, newgen3)
 
-t :: Type -- primer izraza (aplikacija) ; za poiskus zaženi getFormula t
-t = TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun (TypeVar "x") (TypeVar "y") )
+
+tapp :: Type -- primer izraza (aplikacija) ; za poiskus zaženi getFormula tapp
+tapp = TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun (TypeVar "x") (TypeVar "y") )
+
+tcompright :: Type -- primer izraza (desna kompozicija)
+tcompright = TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun ( TypeFun (TypeVar "y") (TypeVar "z") )  ( TypeFun (TypeVar "x") (TypeVar "z") ) )
+
+tcompleft :: Type -- primer izraza (leva kompozicija)
+tcompleft = TypeFun ( TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun (TypeVar "y") (TypeVar "z") ) ) ( TypeFun (TypeVar "x") (TypeVar "z") )
