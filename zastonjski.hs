3a10fec7d158915755c5e1dba06f68a22c1a746e
Domen <domen.monik@gmail.com>
2015-01-03 16:05:23 +0100

    Dodan konstruktor za konstantne tipe

--- zastonjski.hs
+++ zastonjski.hs
@@ -8,12 +8,14 @@ type TypeVariable = String
 
 -- | Tip
 data Type
-	= TypeVar TypeVariable  -- ^ Spremenljivka, ki predstavlja tip
-	| TypeFun Type Type     -- ^ Funkcijski konstruktor tipov ->
-    | TypeList Type         -- ^ Konstruktor za sezname []
+    = TypeVar TypeVariable      -- ^ Spremenljivka
+    | TypeConst TypeVariable    -- ^ Konstantni tip
+    | TypeFun Type Type         -- ^ Funkcijski konstruktor tipov ->
+    | TypeList Type             -- ^ Konstruktor za sezname []
 
 instance Show Type where
     show (TypeVar v) = v
+    show (TypeConst c) = c
     show (TypeList t) = "[" ++ (show t) ++ "]"
     show (TypeFun t1 t2) = case t1 of
         TypeVar t1var -> t1var ++ " -> " ++ (show t2)
@@ -33,7 +35,7 @@ instance Show Term where
         TermVar t2var -> (show t1) ++ " " ++ t2var
         TermApp _ _ -> (show t1) ++ " (" ++ (show t2) ++ ")"
 
--- | Spremenljivka, ki predstavlja relacijo
+-- | Relacijska spremenljivka
 type RelationVariable = String
 
 -- | Osnovna relacija med dvema tipoma
@@ -46,17 +48,19 @@ data Relation = Relation { associatedType :: TypeVariable   -- ^ spremenljivka t
 -- | Logična formula, ki predstavlja zastonjski izrek
 data Formula
 	= ForallRelations RelationVariable (TypeVariable, TypeVariable) Formula -- ^ Kvantificira relacijsko spremenljivko in dva tipa
-	| ForallFunctions TermVariable Type Formula                             -- ^ Kvantificira funkcijsko spremenljivko in funkcijski tip (konstruiran iz TypeFun)
+	| ForallVariables TermVariable Type Formula                             -- ^ Kvantificira izrazovno spremenljivko določenega tipa
 	| ForallPairs (TermVariable, TermVariable) RelationVariable Formula     -- ^ Kvantificira dve izrazovni spremenljivki
 	| IsMember (Term, Term) RelationVariable                                -- ^ Par izrazov je v relaciji
-	| Implication Formula Formula                                           -- ^ Iz prve formule sledi druga formula
+    | Implication Formula Formula                                           -- ^ Iz prve formule sledi druga formula
+    | Equivalence Term Term                                                 -- ^ Izraza sta ekvivalentna
 
 instance Show Formula where
-    show (ForallRelations rv (tv1, tv2) fml) = "Forall " ++ rv ++ " between " ++ tv1 ++ " x " ++ tv2 ++ "\n" ++ (show fml)
-    show (ForallFunctions tv ty fml) = "Forall " ++ tv ++ " :: " ++ (show ty) ++ "\n" ++ (show fml)
+    show (ForallRelations rv (tv1, tv2) fml) = "Forall " ++ rv ++ " between " ++ tv1 ++ " x " ++ tv2 ++ ".\n" ++ (show fml)
+    show (ForallVariables tv ty fml) = "Forall " ++ tv ++ " :: " ++ (show ty) ++ ".\n" ++ (show fml)
     show (ForallPairs (tv1, tv2) rv fml) = "Forall (" ++ tv1 ++ ", " ++ tv2 ++ ") in " ++ rv ++ ". " ++ (show fml)
     show (IsMember (term1, term2) rv) = "(" ++ (show term1) ++ ", " ++ (show term2) ++ ") in " ++ rv
-    show (Implication fml1 fml2) = "(" ++ (show fml1) ++ ") => (" ++ (show fml2) ++ ")"
+    show (Implication fml1 fml2) = "(" ++ (show fml1) ++ ")\n=> (" ++ (show fml2) ++ ")"
+    show (Equivalence term1 term2) = (show term1) ++ " = " ++ (show term2)
 
 -- | Relacija, ki pripada seznamu
 data ListRelation = ListRelation { listrelvar :: RelationVariable   -- ^ Spremenljivka, ki predstavlja relacijo
@@ -93,14 +97,15 @@ nextTermVar vg = let tv = termVarStore vg in (head tv, head $ tail tv, VarGenera
 nextFunVar :: VarGenerator -> (String, String, VarGenerator)
 nextFunVar vg = let fv = funVarStore vg in (head fv, head $ tail fv, VarGenerator {funVarStore = tail $ tail fv, termVarStore = termVarStore vg})
 
--- | Za dani tip vrne pripadajoči zastonjski izrek
-theorem :: Type -> Theorem
-theorem ty = Theorem { theoremFormula = fold relations (fst $ formula (TermVar "f") (TermVar "f") ty varGenerator)
-                        , theoremLRs = listrelations ty
-                        } where
+-- | Za dani tip z danim imenom vrne pripadajoči zastonjski izrek
+theorem :: Type -> String -> Theorem
+theorem ty f = Theorem { theoremFormula = fold relations (fst $ formula (TermVar f) (TermVar f) ty varGenerator)
+                       , theoremLRs = listrelations ty
+                       } where
     allTypeVariables' typ =
         case typ of
             TypeVar x -> [x]
+            TypeConst _ -> []
             TypeList t -> allTypeVariables' t
             TypeFun a b -> nub $ concat [allTypeVariables' a, allTypeVariables' b]
     allTypeVariables = allTypeVariables' ty
@@ -127,16 +132,19 @@ theorem ty = Theorem { theoremFormula = fold relations (fst $ formula (TermVar "
     assocListRelVar typ =
         case typ of
             TypeVar v -> relvar $ associatedRelation v
+            TypeConst c -> "id_" ++ c
             TypeList t -> "[" ++ (assocListRelVar t) ++ "]"
             TypeFun a b -> (assocListRelVar a) ++ " -> " ++ (assocListRelVar b)
     associatedFunction1 typ =
         case typ of
             TypeVar v -> TypeVar $ leftType $ associatedRelation v
+            TypeConst c -> TypeConst c
             TypeList t -> TypeList $ associatedFunction1 t
             TypeFun a b -> TypeFun (associatedFunction1 a) (associatedFunction1 b)
     associatedFunction2 typ =
         case typ of
             TypeVar v -> TypeVar $ rightType $ associatedRelation v
+            TypeConst c -> TypeConst c
             TypeList t -> TypeList $ associatedFunction2 t
             TypeFun a b -> TypeFun (associatedFunction2 a) (associatedFunction2 b)
     varGenerator = VarGenerator { funVarStore = ["p","q","r","s","g","h","k","l","i","j","t","o"] -- Shramba spremenljivk za funkcije
@@ -145,47 +153,60 @@ theorem ty = Theorem { theoremFormula = fold relations (fst $ formula (TermVar "
     formula term1 term2 typ gen =
         case typ of
             TypeVar var -> (IsMember (term1, term2) $ assocRelVar var, gen)
+            TypeConst c -> (Equivalence term1 term2, gen)
             TypeList t -> (IsMember (term1, term2) $ "[" ++ (assocListRelVar t) ++ "]", gen)
             TypeFun (TypeVar avar) b ->
                 let (x, y, newgen) = nextTermVar gen
                     (endFormula, newgen1) = formula (TermApp term1 $ TermVar x) (TermApp term2 $ TermVar y) b newgen
-                in (ForallPairs (x, y) (assocRelVar avar) endFormula, newgen1)
+                in  (ForallPairs (x, y) (assocRelVar avar) endFormula, newgen1)
+            TypeFun (TypeConst c) b ->
+                let (x, y, newgen) = nextTermVar gen
+                    (endFormula, newgen1) = formula (TermApp term1 $ TermVar x) (TermApp term2 $ TermVar x) b newgen
+                in  (ForallVariables x (TypeConst c) endFormula, newgen1)
             TypeFun (TypeList t) b ->
                 let (x, y, newgen) = nextTermVar gen
                     (endFormula, newgen1) = formula (TermApp term1 $ TermVar x) (TermApp term2 $ TermVar y) b newgen
-                in (ForallPairs (x, y) ("[" ++ (assocListRelVar t) ++ "]") endFormula, newgen1)
+                in  (ForallPairs (x, y) ("[" ++ (assocListRelVar t) ++ "]") endFormula, newgen1)
             TypeFun a b ->
                 let (p, q, newgen1) = nextFunVar gen
                     (fstFormula, newgen2) = formula (TermVar p) (TermVar q) a newgen1
                     (sndFormula, newgen3) = formula (TermApp term1 $ TermVar p) (TermApp term2 $ TermVar q) b newgen2
-                in (ForallFunctions p (associatedFunction1 a) $ ForallFunctions q (associatedFunction2 a) $ Implication fstFormula sndFormula, newgen3)
+                in  (ForallVariables p (associatedFunction1 a) $ ForallVariables q (associatedFunction2 a) $ Implication fstFormula sndFormula, newgen3)
     listrelation (TypeList t) =
         let (x, y, gen) = nextTermVar varGenerator
-            alrv = assocListRelVar t
-        in ListRelation { listrelvar = "[" ++ alrv ++ "]"
+            alrv = "[" ++ assocListRelVar t ++ "]"
+        in ListRelation { listrelvar = alrv
                         , headformula = fst $ formula (TermVar x) (TermVar y) t gen
-                        , tailformula =  IsMember ((TermVar "xs"), (TermVar "ys")) alrv
+                        , tailformula = IsMember ((TermVar "xs"), (TermVar "ys")) alrv
                         }
     listrelations typ =
         case typ of
             TypeVar _ -> []
+            TypeConst _ -> []
             TypeList t -> (listrelation typ) : (listrelations t)
             TypeFun a b -> (listrelations a) ++ (listrelations b)
+
+-- | Klic funkcije theorem s privzetim imenom vhodnega tipa "f"
+theorem' :: Type -> Theorem
+theorem' ty = theorem ty "f"
 
 
 -- PRIMERI:
 
-tapp :: Type -- primer izraza (aplikacija) ; za poiskus zaženi  theorem tapp
-tapp = TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun (TypeVar "x") (TypeVar "y") )
+tapplication :: Type -- primer izraza (aplikacija) ; za poiskus zaženi  theorem tapplication "app"
+tapplication = TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun (TypeVar "x") (TypeVar "y") )
 
-tcompright :: Type -- primer izraza (desna kompozicija)
-tcompright = TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun ( TypeFun (TypeVar "y") (TypeVar "z") )  ( TypeFun (TypeVar "x") (TypeVar "z") ) )
+tcompositionr :: Type -- primer izraza (desna kompozicija)
+tcompositionr = TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun ( TypeFun (TypeVar "y") (TypeVar "z") )  ( TypeFun (TypeVar "x") (TypeVar "z") ) )
 
-tcompleft :: Type -- primer izraza (leva kompozicija)
-tcompleft = TypeFun ( TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun (TypeVar "y") (TypeVar "z") ) ) ( TypeFun (TypeVar "x") (TypeVar "z") )
+tcompositionl :: Type -- primer izraza (leva kompozicija)
+tcompositionl = TypeFun ( TypeFun ( TypeFun (TypeVar "x") (TypeVar "y") ) ( TypeFun (TypeVar "y") (TypeVar "z") ) ) ( TypeFun (TypeVar "x") (TypeVar "z") )
 
 tfold :: Type -- fold
 tfold = TypeFun (TypeFun (TypeVar "x") (TypeFun (TypeVar "y") (TypeVar "y"))) (TypeFun (TypeVar "y") (TypeFun (TypeList $ TypeVar "x") (TypeVar "y")))
 
 to :: Type
 to = TypeFun (TypeVar "x") ( TypeFun (TypeFun (TypeVar "y") (TypeList (TypeFun (TypeVar "x") (TypeList $ TypeVar "y")))) (TypeVar "y"))
+
+tfilter :: Type
+tfilter = TypeFun (TypeFun (TypeVar "x") (TypeVar "y")) (TypeFun (TypeConst "Bool") (TypeList $ TypeVar "y"))
